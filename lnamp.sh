c735ea607af14948fdf6bc3747606129999dfda8
vadim2853 <40173027+vadim2853@users.noreply.github.com>
2020-06-22 17:58:41 +0300

    Update lnamp.sh

--- lnamp.sh
+++ lnamp.sh
@@ -9,14 +9,14 @@ rm -r /etc/nginx/sites-available /etc/nginx/sites-enabled /etc/nginx/nginx.conf
 cp -r ./conf/nginx/conf.d/ /etc/nginx/conf.d/
 cp ./conf/nginx/nginx.conf /etc/nginx/nginx.conf
 
-#install apache
+# install apache
 apt-get install -y apache2 libapache2-mod-php
 # replacement of configuration files apache
 rm -r /etc/apache2/ports.conf /etc/apache2/mods-available/dir.conf /etc/apache2/apache2.conf
 cp ./conf/apache/ports.conf /etc/apache2/ports.conf
 cp ./conf/apache/apache2.conf /etc/apache2/apache2.conf
 cp ./conf/apache/mods-available/dir.conf /etc/apache2/mods-available/dir.conf
-#module management
+# module management
 a2dismod mpm_event
 a2enmod mpm_prefork
 a2enmod php7.2
@@ -24,15 +24,15 @@ a2enmod setenvif
 
 # install mariadb
 apt-get install -y mariadb-server
-mysqladmin -u root password
+#mysqladmin -u root password
 
-#install php-fpm
+# install php-fpm
 apt-get install -y php php-fpm
 apt-get install -y php-mysql php-mysqli
 
-#install ftp-Server
+# install ftp-Server
 apt-get install -y proftpd
-ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name=ftp --uid=33 --gid=33 --home=/var/www --shell=/usr/sbin/nologin
+# ftpasswd --passwd --file=/etc/proftpd/ftpd.passwd --name=ftp --uid=33 --gid=33 --home=/var/www --shell=/usr/sbin/nologin
 rm -r /etc/proftpd/proftpd.conf
 cp ./conf/proftpd/proftpd.conf /etc/proftpd/proftpd.conf
 cp ./conf/proftpd/custom.conf /etc/proftpd/conf.d/custom.conf
